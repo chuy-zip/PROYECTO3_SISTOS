ee90d05fa5852e120d818e4ae8a8375ab7fd75ed
chuy-zip <andreschuy03@gmail.com>
2025-05-31 23:06:17 -0600

    checkboxes for schedule and file reading

--- sincronizacion_calendarizacion/schedulingwindow.cpp
+++ sincronizacion_calendarizacion/schedulingwindow.cpp
@@ -1,14 +1,49 @@
 #include "schedulingwindow.h"
 #include "ui_schedulingwindow.h"
 
-SchedulingWindow::SchedulingWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::SchedulingWindow)
-{
-    ui->setupUi(this);
+#include "schedulingwindow.h"
+#include "ui_schedulingwindow.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
-    setWindowTitle("Simulación de Mecanismos de Calendarización");
-    resize(600, 400);
+SchedulingWindow::SchedulingWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::SchedulingWindow) {
+    ui->setupUi(this);
+    setWindowTitle("Simulación de Algoritmos de Calendarización");
+    resize(900, 700);
+
+    // Conectar botones a sus slots
+    connect(ui->btnCargarArchivo, &QPushButton::clicked, this, &SchedulingWindow::onCargarArchivoClicked);
+    connect(ui->btnEjecutarSimulacion, &QPushButton::clicked, this, &SchedulingWindow::onEjecutarSimulacionClicked);
+}
+
+void SchedulingWindow::onCargarArchivoClicked() {
+    QString filePath = QFileDialog::getOpenFileName(this, "Abrir archivo de procesos", "", "Archivos de texto (*.txt)");
+    if (filePath.isEmpty()) return;
+
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Error al abrir el archivo";
+        return;
+    }
+
+    QTextStream in(&file);
+    contenidoArchivo = in.readAll();
+    ui->txtContenidoArchivo->setPlainText(contenidoArchivo); // Mostrar contenido en el QTextEdit
+    file.close();
+}
+
+void SchedulingWindow::onEjecutarSimulacionClicked() {
+    qDebug() << "Contenido del archivo:\n" << contenidoArchivo;
+
+    // Verificar qué algoritmos están seleccionados
+    if (ui->checkBoxFIFO->isChecked()) qDebug() << "Algoritmo FIFO seleccionado";
+    if (ui->checkBoxSJF->isChecked()) qDebug() << "Algoritmo SJF seleccionado";
+    if (ui->checkBoxRR->isChecked()) qDebug() << "Algoritmo SRT seleccionado";
+    if (ui->checkBoxSRT->isChecked()) qDebug() << "Algoritmo Round Robin seleccionado";
+    if (ui->checkBoxPriority->isChecked()) qDebug() << "Algoritmo FIFO seleccionado";
+
 }
 
 SchedulingWindow::~SchedulingWindow()
--- sincronizacion_calendarizacion/schedulingwindow.h
+++ sincronizacion_calendarizacion/schedulingwindow.h
@@ -2,21 +2,26 @@
 #define SCHEDULINGWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class SchedulingWindow;
 }
 
-class SchedulingWindow : public QMainWindow
-{
+class SchedulingWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit SchedulingWindow(QWidget *parent = nullptr);
     ~SchedulingWindow();
 
+private slots:
+    void onCargarArchivoClicked();   // Slot para cargar archivo
+    void onEjecutarSimulacionClicked(); // Slot para simulación
+
 private:
     Ui::SchedulingWindow *ui;
+    QString contenidoArchivo;  // Variable para guardar el contenido del TXT
 };
 
 #endif // SCHEDULINGWINDOW_H
