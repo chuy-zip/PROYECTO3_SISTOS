36facb809a3667853f6fed493a53b9ba361f5262
eunicean <eunice.mata2992@gmail.com>
2025-06-04 14:13:12 -0600

    basic classes

--- sincronizacion_calendarizacion/synchronizationwindow.h
+++ sincronizacion_calendarizacion/synchronizationwindow.h
@@ -2,10 +2,46 @@
 #define SYNCHRONIZATIONWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QVector>
+#include <QMap>
+#include <QTimer>
 
 namespace Ui {
 class SynchronizationWindow;
 }
+
+// clase para los procesos
+struct Process {
+    QString PID;
+    int BT;  // burst Time
+    int AT;  // arrival Time
+    int priority;
+};
+
+// clase para los recursos
+struct Resource {
+    QString name;
+    int count;
+    int available;
+};
+
+// clase para las acciones de cada proceso y con que recurso en que ciclo
+struct Action {
+    QString PID;
+    QString action; // "READ" or "WRITE"
+    QString resource;
+    int cycle;
+};
+
+// autoexplicatorio
+struct SimulationStep {
+    QString PID;
+    QString resource;
+    QString action;
+    int cycle;
+    bool success;
+};
 
 class SynchronizationWindow : public QMainWindow
 {
